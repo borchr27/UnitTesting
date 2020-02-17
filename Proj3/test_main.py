69de1e71c124b22413bc37e9f4370e6b4fc0c529
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-02-17 10:28:59 -0500

    Added unittesting for compound interest function

--- Proj3/test_main.py
+++ Proj3/test_main.py
@@ -1,4 +1,5 @@
 import unittest
+from math import exp
 from main import Interest
 
 class TestInterest(unittest.TestCase):
@@ -28,5 +29,39 @@ class TestInterest(unittest.TestCase):
         self.assertAlmostEqual(Interest().simple_interest(100, .5, 1), 100 * (.5 * 1 + 1))
         self.assertAlmostEqual(Interest().simple_interest(0, .5, 1), 0 * (.5 * 1 + 1))
         self.assertAlmostEqual(Interest().simple_interest(17659, .03, 10), 17659 * (.03 * 10 + 1))
+
+    def test_compound_interest_type_error(self):
+        # Test all arguements of the function to determine that if rouge type is entered that it is caught
+        self.assertRaises(TypeError, Interest().compound_interest, 'init amt', 1, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, 'rate', 1, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, 'time', 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, 1, 'compound num')
+
+
+        self.assertRaises(TypeError, Interest().compound_interest, True, .5, 1, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, True, 1, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, True, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, 1, True)
+
+        self.assertRaises(TypeError, Interest().compound_interest, 1+3j, .5, 1, 2)    
+        self.assertRaises(TypeError, Interest().compound_interest, 100, 1+3j, 1, 2)
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, 1+3j, 2)      
+        self.assertRaises(TypeError, Interest().compound_interest, 100, .5, 1, 1+3j)
+
+
+    def test_compound_interest_value_error(self):
+        # Test all arguments of the function to ensure that negatives, non decimals, and negative time durations dont work
+        self.assertRaises(ValueError, Interest().compound_interest, -100, .5, 1, 2)
+        self.assertRaises(ValueError, Interest().compound_interest, 100, -.5, 1, 2)
+        self.assertRaises(ValueError, Interest().compound_interest, 100, .5, -1, 2)
+        self.assertRaises(ValueError, Interest().compound_interest, 100, 1.5, 1, 2)
+        self.assertRaises(ValueError, Interest().compound_interest, 100, .5, 1, -2)
+
+    
+    def test_compound_output_correctness(self):
+        # Test and make sure the functions are outputting the expected values
+        self.assertAlmostEqual(Interest().compound_interest(100, .5, 1, 2), 100 * (1 + .5 / 2)**(2 * 1))
+        self.assertAlmostEqual(Interest().compound_interest(0, .5, 1, 2), 0 * (1 + .5 / 2)**(2 * 1))
+        self.assertAlmostEqual(Interest().compound_interest(17659, .03, 10, 2), 17659 * (1 + .03 / 2)**(2 * 10))
 
         
